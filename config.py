09af2b0f752eaa76e8efb43f4eedd950c196389f
djromanovdj <39183642+djromanovdj@users.noreply.github.com>
2021-11-01 00:44:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ TOKEN = "2053362792:AAHInkh7hEOLxOHn5I61t3m8skFUuRms9VU"
 
 #Айди администратора бота
 #Айди можно узнать в @myidbot
-ADMIN_ID = #1236426944
+ADMIN_ID = 1236426944
 
 #Айди аккаунта, на который должны приходить логи ошибок
-DEV_ID = #1236426944
+DEV_ID = 1236426944
 
 DATABASE_HOST = "localhost" #адресс базы данных
 DATABASE_PORT = "5432" 
